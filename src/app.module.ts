fa3dcd605415b11e5babb5f514039f3564ea7879
lucascheistwer <lucas.cheistwer@zerf.tech>
2024-12-02 13:14:46 -0300

    Initial Commit, users module, controller, service

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { ConfigModule } from "@nestjs/config";
+import { UsersModule } from "./users/users.module";
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+    UsersModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
