3b3abd8be4508a5203ab142e66db15bba3405c0b
lucascheistwer <lucas.cheistwer@zerf.tech>
2024-12-02 15:25:22 -0300

    feat: auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,16 @@ import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 import { ConfigModule } from "@nestjs/config";
 import { UsersModule } from "./users/users.module";
+import { AuthModule } from "./auth/auth.module";
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     MongooseModule.forRoot(process.env.MONGODB_URI),
     UsersModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,19 +1,33 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { User } from "./schemas/user";
 import { Model } from "mongoose";
 import { CreateUserDto } from "./dto/user.dto";
+import * as bcrypt from "bcrypt";
 
 @Injectable()
 export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
   async create(createUserDto: CreateUserDto): Promise<User> {
-    const createdUser = new this.userModel(createUserDto);
+    const existingUser = await this.findOne(createUserDto.email);
+    if (existingUser) {
+      throw new BadRequestException("User already exists");
+    }
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(createUserDto.password, salt);
+    const createdUser = new this.userModel({
+      ...createUserDto,
+      password: hashedPassword,
+    });
     return createdUser.save();
   }
 
   async findAll(): Promise<User[]> {
     return this.userModel.find().exec();
+  }
+
+  async findOne(email: string): Promise<User> {
+    return this.userModel.findOne({ email }).exec();
   }
 }
